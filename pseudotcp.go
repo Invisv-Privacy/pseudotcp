6347766f0971d1fad120ae7f07ec3aadd266178d
Maxb <bittmanmax@gmail.com>
2024-12-02 12:54:06 -0800

    Fix linting errors
    This deletes a large section of "unused" code. Since this is just a
    skeleton for now, let's begin with a fresh start and lint passing :)

--- pseudotcp.go
+++ pseudotcp.go
@@ -4,7 +4,6 @@ import (
 	"encoding/hex"
 	"errors"
 	"log"
-	"syscall"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
@@ -18,14 +17,14 @@ type SendPacket func(packet []byte, length int) error
 type SocketProtector func(fileDescriptor int) error
 
 var (
-	currentProtect SocketProtector
+	CurrentProtect SocketProtector
 )
 
 var toLinux SendPacket
 
-// Configure sets up a socket protect function to be usable as currentProtect
+// Configure sets up a socket protect function to be usable as CurrentProtect
 func ConfigureProtect(protect SocketProtector) {
-	currentProtect = protect
+	CurrentProtect = protect
 }
 
 func Init(sendPacket SendPacket, proxyFQDN string) error {
@@ -37,9 +36,6 @@ func Send(packetData []byte) {
 	UserStackIPProcessPacket(packetData)
 }
 
-// active indicates whether we can process packets.
-var active bool
-
 // UserStackInit initializes the userspace network stack module. This must be called before any other UserStack function.
 func UserStackInit(proxyFQDN string) error {
 	log.Println("starting")
@@ -48,7 +44,6 @@ func UserStackInit(proxyFQDN string) error {
 		return errors.New("toLinux is nil")
 	}
 
-	active = true
 	return nil
 }
 
@@ -56,7 +51,6 @@ func UserStackInit(proxyFQDN string) error {
 // This is good to call upon detection that a network interface has come back up.
 func ReconnectToProxy(proxyFQDN string) error {
 	log.Println("reconnecting")
-	active = true
 	return nil
 }
 
@@ -64,44 +58,5 @@ func ReconnectToProxy(proxyFQDN string) error {
 func UserStackIPProcessPacket(p []byte) {
 	packet := gopacket.NewPacket(p[:], layers.LayerTypeIPv4, gopacket.Default)
 	log.Println("received packet", packet)
-	log.Println(hex.Dump(p), "\n")
-	return
-}
-
-// pseudoSendToLinux sends a packet to Android/Linux.
-func pseudoSendToLinux(p []byte) error {
-	packet := gopacket.NewPacket(p[:], layers.LayerTypeIPv4, gopacket.Default)
-	log.Println("sending a packet to Linux", packet)
-	log.Println(hex.Dump(p), "\n")
-	if !active {
-		return errors.New("not active")
-	}
-
-	err := toLinux(p, len(p))
-	if err != nil {
-		log.Println("Error sending packet to Linux:", err)
-		return err
-	}
-
-	return nil
-}
-
-func dialerControlProtect(prot SocketProtector) func(network, address string, c syscall.RawConn) error {
-	return func(network, address string, c syscall.RawConn) error {
-		c.Control(func(fd uintptr) {
-			if prot != nil {
-				log.Printf("Protecting FD %v", fd)
-				prot(int(fd))
-				err := syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_RCVBUF, 4194304)
-				if err != nil {
-					log.Printf("Error setting SO_RCVBUF: %v", err)
-				}
-				err = syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, syscall.SO_SNDBUF, 4194304)
-				if err != nil {
-					log.Printf("Error setting SO_SNDBUF: %v", err)
-				}
-			}
-		})
-		return nil // TODO: handle possible errors in doing protect
-	}
+	log.Println(hex.Dump(p))
 }
